427c528306156a750d4b57846454e8b86aa21b75
hongkikii <hyangmeeh@icloud.com>
2024-06-05 22:37:41 +0900

    initial project

---
+++ src/main/java/com/example/csvParsingToDb/CsvParsingToDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvParsingToDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvParsingToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvParsingToDbApplication.class, args);
+	}
+
+}
